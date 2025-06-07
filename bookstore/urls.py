2c7bfc11b7f33ad694284891e43c1da98115e0e0
Tompi-dev <tomiris.kassymova.98@gmail.com>
2025-06-07 23:26:53 +0500

    Added / to not have 404

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def index(request):
+    return JsonResponse({"message": "Welcome to the Bookstore API. Use /api/ to access endpoints."})
+
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+
